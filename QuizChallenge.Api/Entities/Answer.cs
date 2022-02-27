30fa0c9f2c159677fb2808ec0ca2423f700a1ffb
euGen <eugenshala89@gmail.com>
2022-02-27 11:24:12 +0100

    abc

--- QuizChallenge.Api/Entities/Answer.cs
+++ QuizChallenge.Api/Entities/Answer.cs
@@ -13,20 +13,10 @@ namespace QuizChallenge.Api.Entities
         public int Id { get; set; }
         [Required]
         public string Text { get; set; }
-
-      //  public IEnumerable<Answer> Answers { get; set; }
-
-
         public int QuestionId { get; set; }
         [Required]
         [ForeignKey("QuestionId")]
         public virtual Question Question { get; set; }
-
-
-        //public int? QuizId { get; set; }
-        //[Required]
-        //[ForeignKey("QuizId")]
-        //public virtual Quiz Quiz { get; set; }
 
     }
 }
